a9e7e0bb9dcf166d83a22aab39c276ddabb5b1ee
Elvis <elvisnjuguna97@gmail.com>
2024-01-11 13:46:00 +0300

    initial_commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './index.css';
+import Navbar from './components/Navbar'
+//import {Routes} from "react-router-dom";
+
+function App() {
+  return (
+    <div className='App'>
+        <Navbar/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,37 @@
+//import {component} from "react";
+//import React,  { useState } from 'react';
+import "./NavbarStyles.css";
+import { MenuItems } from "./MenuItems";
+//import {Link} from "react-router-dom"
+
+
+function Navbar(){
+    state = {clicked: false};
+    handleClick = () =>{
+        this.setState({clicked: !this.state.clicked})
+    }
+        return(
+            <nav className="NavbarItems">
+                <h1 className="navbar-logo">Food Link</h1>
+
+                <div className="menu-icons" onClick={this.handleClick}>
+                    <i className={this.state.clicked ? "fas fa-times" : "fas fa-bars"}></i>
+                </div>
+
+                <ul className="nav-menu">
+                    {MenuItems.map((item, index) =>{
+                        return(
+                         <li key={index}>
+                            <a className={item.cName} href="/#">
+                            <i className={item.icon}></i>{item.title}
+                            </a>
+                        </li>
+                        )
+                    })}
+                    <button>Sign In</button>
+                </ul>
+            </nav>
+        )
+    }
+
+export default Navbar;
